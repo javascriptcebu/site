f40b518e4f496e078e872002311dde37c7b3a0d0
Dorell James Galang <galangdj@gmail.com>
2018-12-04 21:15:02 +0800

    Updated info

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,10 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <p>Welcome to our humble site.</p>
-    <p>Here we surely started something and we'll make sure it doesn't end here.</p>
-    <a href="https://www.facebook.com/javascriptcebu" target="_blank" rel="noopener noreferrer">Like our Facebook Page</a>
+    <h3>Welcome Cebu Nin.js.</h3>
+    <p>We've just started today out from nothing but to our surprise - within a couple of hours we've already hit <a href="https://www.facebook.com/javascriptcebu/photos/a.593185507781925/593185487781927/" target="_blank" rel="noopener noreferrer">300 Facebook page likes</a></p>
+    <p>How awesome is that? We didn't even paid a single dime for this. We're really seeing a niche here. :)</p>
+    <p>For the meantime as we prepare to launch our official website, please <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A//www.facebook.com/javascriptcebu" target="_blank" rel="noopener noreferrer">continue sharing our Facebook Page</a> or <a href="https://goo.gl/tB4Uud" target="_blank" rel="noopener noreferrer">join us at JavaScript Cebu Slack</a></p>
   </Layout>
 )
 
