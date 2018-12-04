8e87654da54b8d1e3f43da040aee7ad9b8fc8d3c
Dorell James Galang <galangdj@gmail.com>
2018-12-05 00:17:55 +0800

    Make date time sensitive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import moment from 'moment'
 
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
     <h2>Welcome Cebu Nin.js.</h2>
-    <p>We've just started today but to our surprise - within a couple of hours we've already hit <a href="https://www.facebook.com/javascriptcebu/photos/a.593185507781925/593185487781927/" target="_blank" rel="noopener noreferrer">300 Facebook page likes.</a> Yay!</p>
+    <p>We've just started <span title="December 04, 2018" style={{ borderStyle: "dashed", border: 0, borderBottom: "3px dashed #333" }}>{moment('12/04/18').fromNow()}</span> but to our surprise - within a couple of hours we've already hit <a href="https://www.facebook.com/javascriptcebu/photos/a.593185507781925/593185487781927/" target="_blank" rel="noopener noreferrer">300 Facebook page likes.</a> Yay!</p>
     <p><em>UPDATE: We're 400+ likes now and we're really growing...</em></p>
 
     <h3>Another JavaScript Group?</h3>
