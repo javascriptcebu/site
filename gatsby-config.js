b4cf78c4df2f9815e991fc5c641639c78141e476
Dorell James Galang <galangdj@gmail.com>
2018-12-04 20:51:21 +0800

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteMetadata: {
+    title: 'JavaScript Cebu',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#f7df1e',
+        theme_color: '#2c251b',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <p>Welcome to our humble site.</p>
+    <p>Here we surely started something and we'll make sure it doesn't end here.</p>
+    <a href="https://www.facebook.com/javascriptcebu" target="_blank" rel="noopener noreferrer">Like our Facebook Page</a>
+  </Layout>
+)
+
+export default IndexPage
